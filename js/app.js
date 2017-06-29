9165372c2ca5c23425a24844f1e9d27f10f11fa3
Smilzy <4wozniak@gmail.com>
2017-06-29 22:52:56 +0200

    Implemented deleting items. Fixed spelling error

--- js/app.js
+++ js/app.js
@@ -23,14 +23,14 @@ app.service("GroceryService", function() {
 	var groceryService = {};
 
 	groceryService.groceryItems = [
-		{id:1, completed: true, itemName: 'milk', date: '01-06-2017'},
-		{id:2, completed: true, itemName: 'bread', date: '01-06-2017'},
-		{id:3, completed: true, itemName: 'meat', date: '02-06-2017'},
-		{id:4, completed: true, itemName: 'ice cream', date: '03-06-2017'},
-		{id:5, completed: true, itemName: 'tomatoes', date: '04-06-2017'},
-		{id:6, completed: true, itemName: 'black zero', date: '05-06-2017'},
-		{id:7, completed: true, itemName: 'OCB', date: '06-06-2017'},
-		{id:8, completed: true, itemName: 'KitKat', date: '06-06-2017'}
+		{id:1, completed: true, itemName: 'milk', date: new Date('June 1, 2017 16:20:00')},
+		{id:2, completed: true, itemName: 'bread', date: new Date('June 2, 2017 13:29:00')},
+		{id:3, completed: true, itemName: 'meat', date: new Date('June 2, 2017 14:55:00')},
+		{id:4, completed: true, itemName: 'ice cream', date: new Date('June 3, 2017 08:00:00')},
+		{id:5, completed: true, itemName: 'tomatoes', date: new Date('June 3, 2017 09:20:00')},
+		{id:6, completed: true, itemName: 'black zero', date: new Date('June 4, 2017 23:55:00')},
+		{id:7, completed: true, itemName: 'OCB', date: new Date('June 5, 2017 00:20:00')},
+		{id:8, completed: true, itemName: 'KitKat', date: new Date('June 5, 2017 19:30:00')}
 	];
 
 	groceryService.findById = function(id) {
@@ -51,7 +51,14 @@ app.service("GroceryService", function() {
 		}
 	};
 
+	groceryService.removeItem = function(entry){
+		var index = groceryService.groceryItems.indexOf(entry);
+
+		groceryService.groceryItems.splice(index, 1); // start at INDEX and delete 1
+	};
+
 	groceryService.save = function(entry) {
+
 		var updateItem = groceryService.findById(entry.id);
 
 		if(updateItem) {
@@ -71,6 +78,9 @@ app.service("GroceryService", function() {
 app.controller("HomeController", ["$scope", "GroceryService", function($scope, GroceryService) {
 	$scope.appTitle = "Grocery List";
 	$scope.groceryItems = GroceryService.groceryItems;
+	$scope.removeItem = function(entry) {
+		GroceryService.removeItem(entry);
+	}
 }]);
 
 app.controller("GroceryListItemController", ["$scope", "$routeParams", "$location", "GroceryService", function($scope, $routeParams, $location, GroceryService) {	
