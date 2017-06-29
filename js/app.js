2515b2f23632f1c772ba02c1a091fc1b6b85bc8b
Smilzy <4wozniak@gmail.com>
2017-06-29 21:09:24 +0200

    Add grocery item functionality

--- js/app.js
+++ js/app.js
@@ -24,15 +24,19 @@ app.service("GroceryService", function() {
 	var groceryService = {};
 
 	groceryService.groceryItems = [
-		{completed: true, itemName: 'milk', date: '01-06-2017'},
-		{completed: true, itemName: 'bread', date: '01-06-2017'},
-		{completed: true, itemName: 'meat', date: '02-06-2017'},
-		{completed: true, itemName: 'ice cream', date: '03-06-2017'},
-		{completed: true, itemName: 'tomatoes', date: '04-06-2017'},
-		{completed: true, itemName: 'black zero', date: '05-06-2017'},
-		{completed: true, itemName: 'OCB', date: '06-06-2017'},
-		{completed: true, itemName: 'KitKat', date: '06-06-2017'}
-	]
+		{id:1, completed: true, itemName: 'milk', date: '01-06-2017'},
+		{id:2, completed: true, itemName: 'bread', date: '01-06-2017'},
+		{id:3, completed: true, itemName: 'meat', date: '02-06-2017'},
+		{id:4, completed: true, itemName: 'ice cream', date: '03-06-2017'},
+		{id:5, completed: true, itemName: 'tomatoes', date: '04-06-2017'},
+		{id:6, completed: true, itemName: 'black zero', date: '05-06-2017'},
+		{id:7, completed: true, itemName: 'OCB', date: '06-06-2017'},
+		{id:8, completed: true, itemName: 'KitKat', date: '06-06-2017'}
+	];
+	
+	groceryService.save = function(entry) {
+		groceryService.groceryItems.push(entry);
+	};
 
 	return groceryService;
 })
@@ -41,8 +45,15 @@ app.controller("HomeController", ["$scope", function($scope) {
 	$scope.appTitle = "Grocery List";
 }]);
 
-app.controller("GroceryListItemsController", ["$scope", "$routeParams", "GroceryService", function($scope, $routeParams, GroceryService) {
+app.controller("GroceryListItemsController", ["$scope", "$routeParams", "$location", "GroceryService", function($scope, $routeParams, $location, GroceryService) {
+	
 	$scope.groceryItems = GroceryService.groceryItems;
 
-	$scope.rp = "Route Parameter Value: " + $routeParams.id + " " + $routeParams.cat; 
+	$scope.groceryItem = { id:7, completed:true, itemName: "cheese", date: new Date() }
+
+	$scope.save = function() {
+		GroceryService.save( $scope.groceryItem );
+		$location.path("/");
+	}
+
 }]);
